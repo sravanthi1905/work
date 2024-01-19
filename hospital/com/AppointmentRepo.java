b95395e2450a7302a06be2297ae7339a96ffbcb5
sravanthi1905 <85628487+sravanthi1905@users.noreply.github.com>
2024-01-19 19:14:49 +0530

    Add files via upload

---
+++ hospital/com/AppointmentRepo.java
@@ -0,0 +1,23 @@
+package hospital.com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import hospital.com.Appointment;
+import hospital.com.Doctor;
+
+
+@Repository
+public class AppointmentRepo {
+		@Autowired
+		JdbcTemplate jdbcTemplate;
+		
+		public void save(Appointment app) {
+			String sql = "insert into Appointment values(?,?,?)";
+			jdbcTemplate.update(sql, new Object[] {app.getAppId(),app.getAppDate(),app.getPatient().getPatientId()});
+			
+		
+		}
+	}
+
